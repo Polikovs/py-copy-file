717591850152e1ee9c8b022ef109de2b51372900
Oleksandr Polikovskiy <trotpolik@gmail.com>
2026-02-07 18:35:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    temp = command.split()
+    if len(temp) == 3:
+        command, old_file, new_file = temp
+        if old_file != new_file and command == "cp":
+            try:
+                with (open(old_file, "r") as file_in,
+                      open(new_file, "w") as file_out):
+                    file_out.write(file_in.read())
+            except FileNotFoundError:
+                print("File not found")
